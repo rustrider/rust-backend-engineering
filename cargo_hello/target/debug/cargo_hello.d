4d840a3c9d57888b2c7d343543c415b210181786
youssefhoumazen <chainmaven@proton.me>
2025-02-07 15:30:10 +0100

    chapter:1 stream#01

---
+++ cargo_hello/target/debug/cargo_hello.d
@@ -0,0 +1,1 @@
+/Users/youssefhoumazen/Desktop/rust-backend-engineering/cargo_hello/target/debug/cargo_hello: /Users/youssefhoumazen/Desktop/rust-backend-engineering/cargo_hello/src/main.rs
---
+++ cargo_hello/target/debug/deps/cargo_hello-36c75b5b42008306.d
@@ -0,0 +1,5 @@
+/Users/youssefhoumazen/Desktop/rust-backend-engineering/cargo_hello/target/debug/deps/cargo_hello-36c75b5b42008306: src/main.rs
+
+/Users/youssefhoumazen/Desktop/rust-backend-engineering/cargo_hello/target/debug/deps/cargo_hello-36c75b5b42008306.d: src/main.rs
+
+src/main.rs:
