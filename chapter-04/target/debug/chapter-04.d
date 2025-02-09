2ddd6c49bdb18e5bcc4d6e5a5db57f4a9069c87a
youssefhoumazen <chainmaven@proton.me>
2025-02-09 15:56:27 +0100

    adding some resources/resume

--- chapter-04/target/debug/chapter-04.d
+++
@@ -1,1 +0,0 @@
-/Users/youssefhoumazen/Desktop/rust-backend-engineering/chapter-04/target/debug/chapter-04: /Users/youssefhoumazen/Desktop/rust-backend-engineering/chapter-04/src/main.rs
--- chapter-04/target/debug/deps/chapter_04-4e14de30ab103aa3.d
+++
@@ -1,5 +0,0 @@
-/Users/youssefhoumazen/Desktop/rust-backend-engineering/chapter-04/target/debug/deps/chapter_04-4e14de30ab103aa3: src/main.rs
-
-/Users/youssefhoumazen/Desktop/rust-backend-engineering/chapter-04/target/debug/deps/chapter_04-4e14de30ab103aa3.d: src/main.rs
-
-src/main.rs:
